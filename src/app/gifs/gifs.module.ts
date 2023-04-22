a7ac88ff555c8d9d190fd06bc43d46cba6e47527
RubenRGanga <rubenruizganga@gmail.com>
2023-04-23 00:43:26 +0200

    añadido spinner y principios SOLID

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SharedModule } from '../shared/shared.module'
+
 import { CardListComponent } from './components/card-list/card-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ImgCardComponent } from './components/img-card/img-card.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent,
-    SearchBoxComponent,
     CardListComponent,
+    HomePageComponent,
+    ImgCardComponent,
+    SearchBoxComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports: [
     HomePageComponent,
