d23077cc77650be2b06516c7663bbdb302baedd1
RubenRGanga <rubenruizganga@gmail.com>
2023-04-22 21:51:48 +0200

    ngfor url gifs

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+  constructor(private http: HttpClient) { }
+
+  private _tagsHistory: string[] = [];
+  private apiKey: string = "MM7k4wyskrhxHkUzdLhsS0hGajHH4JnR"
+  private apiUrl: string = 'https://api.giphy.com/v1/gifs'
+
+  public gifList: Gif[] = [];
+
+  get tagsHistory() {
+    return [...this._tagsHistory]
+  }
+
+  private orderHistory(tag: string){
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes( tag )) {
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag)
+    }
+
+    this._tagsHistory.unshift(tag)
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+  }
+
+  seachTag(tag: string):void {
+    this.orderHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      // .set('limit', '100')
+      // .set('rating', 'g')
+      .set('lang', 'en')
+
+    this.http.get<SearchResponse>(`${this.apiUrl}/search`, { params })
+      .subscribe( resp => {
+
+        this.gifList = resp.data;
+
+      });
+
+
+  }
+
+}
